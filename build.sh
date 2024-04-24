e056e896691faa8f18f048545100c6b2ea8c6510
Shetty Yttehs <yttehs@ROG-Strix-G16>
2024-04-24 19:11:33 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#  _                    
+# | |__   _____  _____  
+# | '_ \ / _ \ \/ / _ \ 
+# | | | |  __/>  < (_) |
+# |_| |_|\___/_/\_\___/ 
+
+# +-------------------+
+# | hexo build script |
+# +-------------------+
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+RESET='\033[0m'
+
+# install hexo if not found
+if ! [ command -v hexo ]; then
+	echo -e "${RED}hexo not found${RESET}"
+	npm install -g hexo-cli
+	npm install
+fi
+
+# build site
+echo -e "${GREEN}Building site...${RESET}"
+hexo generate
