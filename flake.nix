84060fe5219238914a0744e73100b32d3a80f59b
Shetty Yttehs <shetty.yttehs@outlook.com>
2025-10-17 17:34:24 +0800

    build(flake): add nix dev shell config
    add direnv config

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Dev shell for hexo project";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ hexo-cli nodejs_24 python314 ];
+
+        shellHook = ''
+          echo "Nix dev shell"
+        '';
+      };
+    };
+}
+
