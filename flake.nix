779549603b2fc2a42879063ca5d9f4d84a836289
Shetty Yttehs <shetty.yttehs@outlook.com>
2025-12-03 10:03:11 +0800

    chore(flake): update nixpkgs to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Dev shell for hexo project";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
   outputs = { self, nixpkgs }:
     let
